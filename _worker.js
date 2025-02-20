f8c6118600d9a6490bc78e20c341b82d6e279943
leotvgo <160683122+leotvgo@users.noreply.github.com>
2025-02-20 16:38:19 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -24,7 +24,7 @@ https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub
 
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+let subConfig = "https://雷霆.us.kg/https://raw.githubusercontent.com/leotvgo/duo/refs/heads/main/guize/leo.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
